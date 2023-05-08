eeed95c81cb5d6b12e9d5b70aa2afef3fd6c7523
Lenovo <Lenovo@DESKTOP-SHTM9GH>
2023-05-08 23:05:05 +0400

    Добавьте файлы проекта.

---
+++ InterfaceProj/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace InterfaceProj
+{
+    using InterfaceProj;
+    using System.Windows.Input;
+
+    public class Interface
+    {
+
+
+
+        Fighter fighter = new Fighter();
+        List<IFighter> fighters = new List<IFighter>();
+        List<IKiller> killers = new List<IKiller>();
+    Commander commander = new Commander();
+     List<IManage> manages = new List<IManage>();
+       
+}
+         public class Fighter : IFighter, IKiller
+    {
+
+        public void canAttack()
+        {
+            Console.WriteLine("Fighter can attack");
+        }
+
+        public void canFire()
+        {
+            Console.WriteLine("Fighter can attack");
+        }
+
+        public void canKill()
+        {
+            Console.WriteLine("Killer can attack");
+        }
+
+        public void canShot()
+        {
+            Console.WriteLine("Fighter can attack");
+        }
+    }
+    public class Commander : IFighter, IKiller, IManage
+    {
+        void IFighter.canAttack()
+        {
+            Console.WriteLine("Fighter can attack");
+        }
+
+        void IFighter.canFire()
+        {
+            Console.WriteLine("Fighter can attack");
+        }
+
+        void IKiller.canKill()
+        {
+            Console.WriteLine("Fighter can attack");
+        }
+
+        void IFighter.canShot()
+        {
+            Console.WriteLine("Fighter can attack");
+        }
+
+        void IManage.completeMission()
+        {
+            Console.WriteLine("Manage can attack");
+        }
+
+        void IManage.control()
+        {
+            Console.WriteLine("Manage can attack");
+        }
+
+        void IManage.gatherPeople()
+        {
+            Console.WriteLine("Manage can attack");
+        }
+    }
+}
+        
